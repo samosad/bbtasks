869615521b7329f4ab05931e6b2242ac97a9aef8
Aleksey Tabakman <samosad.ru@gmail.com>
2013-11-27 00:31:12 +0400

    added views (step 3.5)

---
+++ app/js/views/auth.js
@@ -0,0 +1,30 @@
+define([
+  'text!templates/auth.html'
+],
+
+function(template) {
+  var AuthView = Backbone.View.extend({
+    el: '#sign-in-container',
+    template: _.template(template),
+
+    events: {
+      'clicl #authorize-button': 'auth'
+    },
+
+    initialize: function(app) {
+      this.app = app;
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+
+    auth: function() {
+      this.app.apiManager.checkAuth();
+      return false;
+    }
+  });
+
+  return AuthView;
+});
